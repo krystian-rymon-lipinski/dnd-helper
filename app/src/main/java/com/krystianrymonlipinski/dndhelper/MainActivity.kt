578caa5f4c3d01550697b98f2f77253d19ef59ad
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2025-08-12 17:08:13 +0200

    Add dummy values

--- app/src/main/java/com/krystianrymonlipinski/dndhelper/MainActivity.kt
+++ app/src/main/java/com/krystianrymonlipinski/dndhelper/MainActivity.kt
@@ -7,18 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.krystianrymonlipinski.dndhelper.model.CharacterState
 import com.krystianrymonlipinski.dndhelper.ui.character_sheet.CharacterSheetScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val myCharacterState = CharacterState()
+
         setContent {
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CharacterSheetScreen()
+                    CharacterSheetScreen(Modifier, myCharacterState)
                 }
             }
         }
--- app/src/main/java/com/krystianrymonlipinski/dndhelper/ui/character_sheet/CharacterSheetScreen.kt
+++ app/src/main/java/com/krystianrymonlipinski/dndhelper/ui/character_sheet/CharacterSheetScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -22,63 +23,65 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.krystianrymonlipinski.dndhelper.R
+import com.krystianrymonlipinski.dndhelper.model.CharacterAbilityScoresState
+import com.krystianrymonlipinski.dndhelper.model.CharacterBasicsState
+import com.krystianrymonlipinski.dndhelper.model.CharacterState
 
 @Composable
-fun CharacterSheetScreen(modifier: Modifier = Modifier) {
+fun CharacterSheetScreen(
+    modifier: Modifier = Modifier,
+    characterState: CharacterState = CharacterState()
+) {
     Column(
         modifier = modifier.fillMaxSize()
     ) {
         RowTitle(stringRes = R.string.basics_title, modifier = modifier)
-        CharacterBasics()
+        CharacterBasics(characterBasicsState = characterState.basicsState)
         HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
 
         RowTitle(stringRes = R.string.ability_scores_title, modifier = modifier)
-        CharacterAbilityScores()
+        CharacterAbilityScores(abilityScoresState = characterState.abilityScoresState)
         HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
     }
 }
 
 
 @Composable
-fun CharacterBasics(modifier: Modifier = Modifier) {
+fun CharacterBasics(
+    modifier: Modifier = Modifier,
+    characterBasicsState: CharacterBasicsState
+) {
     Row(
         modifier = modifier
             .fillMaxWidth(1f)
-            .fillMaxHeight(0.15f)
+            .wrapContentHeight()
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        NumberWithTextVertically(number = 0, text = stringResource(id = R.string.stat_hp))
-        NumberWithTextVertically(number = 0, text = stringResource(id = R.string.stat_ac))
-        NumberWithTextVertically(number = 0, text = stringResource(id = R.string.stat_initiative))
-        NumberWithTextVertically(number = 0, text = stringResource(id = R.string.stat_speed))
+        NumberWithTextVertically(number = characterBasicsState.hitPoints, text = stringResource(id = R.string.stat_hp))
+        NumberWithTextVertically(number = characterBasicsState.armorClass, text = stringResource(id = R.string.stat_ac))
+        NumberWithTextVertically(number = characterBasicsState.initiative, text = stringResource(id = R.string.stat_initiative))
+        NumberWithTextVertically(number = characterBasicsState.speed, text = stringResource(id = R.string.stat_speed))
     }
 }
 
 @Composable
-fun CharacterAbilityScores() {
+fun CharacterAbilityScores(abilityScoresState: CharacterAbilityScoresState) {
     Row(
         modifier = Modifier
             .fillMaxWidth(1f)
-            .fillMaxHeight(0.15f)
-            .padding(8.dp),
+            .wrapContentHeight()
+            .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        val strength = 16
-        val dexterity = 14
-        val constitution = 15
-        val intelligence = 12
-        val wisdom = 10
-        val charisma = 8
-
-        NumberWithTextVertically(number = strength, text = stringResource(id = R.string.ability_str))
-        NumberWithTextVertically(number = dexterity, text = stringResource(id = R.string.ability_dex))
-        NumberWithTextVertically(number = constitution, text = stringResource(id = R.string.ability_con))
-        NumberWithTextVertically(number = intelligence, text = stringResource(id = R.string.ability_int))
-        NumberWithTextVertically(number = wisdom, text = stringResource(id = R.string.ability_wis))
-        NumberWithTextVertically(number = charisma, text = stringResource(id = R.string.ability_cha))
+        NumberWithTextVertically(number = abilityScoresState.strength, text = stringResource(id = R.string.ability_str))
+        NumberWithTextVertically(number = abilityScoresState.dexterity, text = stringResource(id = R.string.ability_dex))
+        NumberWithTextVertically(number = abilityScoresState.constitution, text = stringResource(id = R.string.ability_con))
+        NumberWithTextVertically(number = abilityScoresState.intelligence, text = stringResource(id = R.string.ability_int))
+        NumberWithTextVertically(number = abilityScoresState.wisdom, text = stringResource(id = R.string.ability_wis))
+        NumberWithTextVertically(number = abilityScoresState.charisma, text = stringResource(id = R.string.ability_cha))
     }
 }
 
